e2f540e9736d07470d82070d9721fc15abc72df2
Il tuo nome <la_tua_email@example.com>
2024-06-10 21:59:16 +0200

    Set up - and working on structure (following Riccardo's lesson)

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import MainComponent from './components/MainComponent.vue';
+  import FooterComponent from './components/FooterComponent.vue';
+
+
+ 
+
+</script>
+
+<template>
+  <HeaderComponent></HeaderComponent>
+  <MainComponent></MainComponent>
+  <FooterComponent/>
+</template>
+
+<style lang="scss">
+
+@use './styles/general.scss';
+@use './styles/partials/variables';
+@use './styles/partials/mixin'
+
+</style>
---
+++ my-vue-app/src/components/HeaderComponent.vue
@@ -0,0 +1,62 @@
+<script>
+    export default {
+        data(){
+            return {
+
+                headerLinks: [
+                { title: 'Characters', id: 1, active: false },
+                { title: 'Comics', id: 2, active: false },
+                { title: 'Movies', id: 3, active: false },
+                { title: 'TV', id: 4, active: false },
+                { title: 'Games', id: 5, active: false },
+                { title: 'Collectibles', id: 6, active: false },
+                { title: 'Videos', id: 7, active: false },
+                { title: 'Fans', id: 8, active: false },
+                { title: 'News', id: 9, active: false },
+                { title: 'Shop', id: 10, active: false }
+            ]
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+ 
+
+    <header class="d-flex">
+       
+        <section class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </section>
+
+        <section class="navbar">
+            
+                <nav>
+                    <ul>
+                     
+                        <li >
+                            {{ headerLinks[0] }}
+                        </li>
+
+                    </ul>
+                </nav>
+
+        </section>
+
+    </header>
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+   @use '../styles/partials/variables' as *;
+
+   li.active{
+    background-color: $active-link-bg;
+   }
+    
+</style>
